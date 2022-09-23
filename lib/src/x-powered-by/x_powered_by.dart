d5dbdaf38fa97cc4a5af8819b8a6c5443987511f
jxstxn__ <justin.baumann@t-online.de>
2022-09-23 21:02:36 +0200

    Simplify way of removing the header

--- lib/src/x-powered-by/x_powered_by.dart
+++ lib/src/x-powered-by/x_powered_by.dart
@@ -4,12 +4,13 @@ Middleware xPoweredBy() {
   return (innerHandler) {
     return (request) async {
       final response = await innerHandler(request);
-      // We need to remove the header in the updated list...
-      final headers = {...response.headersAll}..remove('X-Powered-By');
+      final headers = {...response.headersAll}..update(
+          'X-Powered-By',
+          (_) => [],
+          ifAbsent: () => [],
+        );
       return response.change(
         headers: {
-          // ... and set it here to null, to actually remove it.
-          'X-Powered-By': null,
           ...headers,
         },
       );
