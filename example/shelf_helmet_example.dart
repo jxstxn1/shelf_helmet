bed8d2d066ae7832d28c93474435807525ae894c
jxstxn__ <justin.baumann@t-online.de>
2022-09-23 17:21:55 +0200

    Init

---
+++ example/shelf_helmet_example.dart
@@ -0,0 +1,6 @@
+import 'package:shelf_helmet/shelf_helmet.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/shelf_helmet.dart
@@ -0,0 +1,4 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library shelf_helmet;
