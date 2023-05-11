01ca9794af6388ee289e6b345be799172cc89254
Justin Baumann <justin.baumann@t-online.de>
2023-05-11 22:08:38 +0200

    test new behaviour

--- test/middlewares/helmet_test.dart
+++ test/middlewares/helmet_test.dart
@@ -21,24 +21,7 @@ void main() {
 
     expect(response.statusCode, 200);
     expect(response.headers['content-type'], 'application/json');
-    expect(
-      response.headers['content-security-policy'],
-      "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
-    );
-    expect(response.headers['cross-origin-opener-policy'], 'same-origin');
-    expect(response.headers['cross-origin-resource-policy'], 'same-origin');
-    expect(response.headers['origin-agent-cluster'], '?1');
-    expect(response.headers['referrer-policy'], 'no-referrer');
-    expect(
-      response.headers['strict-transport-security'],
-      'max-age=15552000; includeSubDomains',
-    );
-    expect(response.headers['x-content-type-options'], 'nosniff');
-    expect(response.headers['x-dns-prefetch-control'], 'off');
-    expect(response.headers['x-download-options'], 'noopen');
-    expect(response.headers['x-frame-options'], 'SAMEORIGIN');
-    expect(response.headers['x-permitted-cross-domain-policies'], 'none');
-    expect(response.headers['x-xss-protection'], '0');
+    defaultHelmetOptionsExpecter(response);
   });
 
   test(
@@ -87,4 +70,29 @@ void main() {
       );
     },
   );
+  test('CrossOriginEmbedderPolicy can be activated with the options', () async {
+    final handler = const Pipeline()
+        .addMiddleware(
+          helmet(
+            options: const HelmetOptions(enableCrossOriginEmbedderPolicy: true),
+          ),
+        )
+        .addHandler(
+          (req) => syncHandler(
+            req,
+            headers: {'content-type': 'application/json'},
+          ),
+        );
+
+    final response = await makeRequest(
+      handler,
+      uri: clientUri,
+      method: 'GET',
+    );
+
+    expect(response.statusCode, 200);
+    expect(response.headers['content-type'], 'application/json');
+    defaultHelmetOptionsExpecter(response);
+    expect(response.headers['cross-origin-embedder-policy'], 'require-corp');
+  });
 }
--- test/utils/test_utils.dart
+++ test/utils/test_utils.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:shelf/shelf.dart';
+import 'package:test/test.dart';
 
 final Uri clientUri = Uri.parse('http://127.0.0.1:4322/');
 
@@ -41,4 +42,25 @@ Future<Response> makeRequest(
       );
     },
   );
+}
+
+void defaultHelmetOptionsExpecter(Response response) {
+  expect(
+    response.headers['content-security-policy'],
+    "default-src 'self';base-uri 'self';font-src 'self' https: data:;form-action 'self';frame-ancestors 'self';img-src 'self' data:;object-src 'none';script-src 'self';script-src-attr 'none';style-src 'self' https: 'unsafe-inline';upgrade-insecure-requests",
+  );
+  expect(response.headers['cross-origin-opener-policy'], 'same-origin');
+  expect(response.headers['cross-origin-resource-policy'], 'same-origin');
+  expect(response.headers['origin-agent-cluster'], '?1');
+  expect(response.headers['referrer-policy'], 'no-referrer');
+  expect(
+    response.headers['strict-transport-security'],
+    'max-age=15552000; includeSubDomains',
+  );
+  expect(response.headers['x-content-type-options'], 'nosniff');
+  expect(response.headers['x-dns-prefetch-control'], 'off');
+  expect(response.headers['x-download-options'], 'noopen');
+  expect(response.headers['x-frame-options'], 'SAMEORIGIN');
+  expect(response.headers['x-permitted-cross-domain-policies'], 'none');
+  expect(response.headers['x-xss-protection'], '0');
 }
